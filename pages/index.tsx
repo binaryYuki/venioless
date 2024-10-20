627c74a0a212a34ecd1758d91e1cfa36e117b35b
binaryYuki <60097976+binaryYuki@users.noreply.github.com>
2024-10-20 10:15:28 +0100

    feat: domain redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,12 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => ({
 });
 
 export const checkToken = async () => {
-  // if request.domain = venioless.vercel.app jump to venioless.vercel.app
   if (window.location.hostname === "venioless.vercel.app") {
     window.location.href = "https://venioless.tzpro.uk";
   }
   if (window.location.hostname === "localhost") {
     window.location.href = "https://venioless.tzpro.uk";
   }
-  console.log(window.location.hostname);
   const token = localStorage.getItem("token");
 
   if (token) {
